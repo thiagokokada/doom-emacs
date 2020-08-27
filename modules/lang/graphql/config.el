01802ddd03c7dc0c53c03bea846e5b96753b97f5
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-08-26 21:18:07 -0300

    modules/lang: Add GraphQL module
    Based on e24b8cd1d1e4ceb7e4454a267985b639d5e7eb06, the only difference
    is support for `.graphql` extension.
    
    Also, cleanup core/autoload/help.el, it was pointing for support for
    some files in `data` module that is not support since the commit above,
    and was making me confused about why GraphQL was not working.

---
+++ modules/lang/graphql/config.el
@@ -0,0 +1,5 @@
+;;; lang/graphql/config.el -*- lexical-binding: t; -*-
+
+(use-package! graphql-mode
+  :mode ("\\.gql\\'" "\\.graphql\\'")
+  :config (setq-hook! 'graphql-mode-hook tab-width graphql-indent-level))
